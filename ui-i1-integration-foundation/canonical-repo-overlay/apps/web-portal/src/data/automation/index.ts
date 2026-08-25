ad24ddd12ae3e576e6da0fe704f72634334eee06
DuyVuux <vuduy280804@gmail.com>
2026-08-25 13:14:37 +0700

    Remove mistakenly added ui-i1-integration-foundation directory

--- ui-i1-integration-foundation/canonical-repo-overlay/apps/web-portal/src/data/automation/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./automation-repository";
-export * from "./real-automation-repository";
-export * from "./mock-automation-repository";
-export * from "./repository-factory";
-export * from "./automation-context";
